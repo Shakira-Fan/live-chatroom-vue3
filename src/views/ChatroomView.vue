49cc8e9ce763e66091c1053e1faf796abbdca583
Shakira0612 <105627687+Shakira0612@users.noreply.github.com>
2022-10-21 14:18:55 +0800

    create nav bar

--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -1,10 +1,14 @@
 <template>
-<div>chat room</div>
+  <div class="container">
+    <NavBar/>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar";
 export default {
-  name: "ChatroomView"
+  name: "ChatroomView",
+  components: {NavBar}
 }
 </script>
 
