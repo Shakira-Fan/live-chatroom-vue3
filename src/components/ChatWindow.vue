0e8de5f39e89583a63f60252783eedce2e6be4c0
Shakira0612 <105627687+Shakira0612@users.noreply.github.com>
2022-10-24 14:51:00 +0800

    build and deploy firebase

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div class="messages" v-if="documents">
+    <div class="messages" v-if="documents" ref="messages">
       <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
         <span class="created-at">{{ doc.createdAt }}</span>
         <span class="name">{{ doc.name }}</span>
@@ -14,7 +14,7 @@
 <script>
 import getCollection from "@/composables/getCollection";
 import {formatDistanceToNow} from 'date-fns';
-import {computed} from "vue";
+import {computed, onUpdated, ref} from "vue";
 
 export default {
   name: "ChatWindow",
@@ -30,7 +30,15 @@ export default {
       }
     })
 
-    return {error, documents,formattedDocuments}
+    //auto-scroll to bottom of messages
+    const messages=ref(null)
+
+    onUpdated(()=>{
+      messages.value.scrollTop = messages.value.scrollHeight
+    })
+
+
+    return {error, documents,formattedDocuments,messages}
   }
 }
 </script>
